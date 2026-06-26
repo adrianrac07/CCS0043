ca8a2760fc0a66e789066833b0f4898bbbd55af6
adrianrovic07 <adrianrovic07@gmail.com>
2026-06-26 22:08:31 +0800

    wdw

--- act2/index.php
+++ act2/index.php
@@ -1,11 +1,11 @@
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- </head>
- <body>
+</head>
+<body>
    <form action="welcome.php" method="post">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
@@ -14,5 +14,5 @@ E-mail: <input type="text" name="email"><br>
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 
- </body>
- </html>
+   </body>
+   </html> 
